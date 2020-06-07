e49d27bf8d2986758aed1985d37f9e00dceca47b
vfhwang <billy@Billys-iMac.local>
2020-06-07 16:17:11 +0100

    added slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,36 @@
 $(window).load(function() {
-    $('.flexslider').flexslider({
-    controlNav: false,
-    directionNav: true,
-    touch: true,
-    animation: "slide",
-    controlsContainer: $(".custom-controls-container"),
-    customDirectionNav: $(".custom-navigation a")
-    });
+
+    if($(window).width() >= 600){
+        console.log('HI HI HI');
+        $('.flexslider').flexslider({
+            controlNav: false,
+            directionNav: true,
+            touch: true,
+            animation: "slide",
+            controlsContainer: $(".custom-controls-container"),
+            customDirectionNav: $(".custom-navigation a"),
+            slideshow: false,
+            });
+      }else{
+        var el = document.getElementById('delete-on-mob');
+        el.remove(); // Removes the div with the 'div-02' id
+      };
+
 });
+
+
+
+// $(window).resize(function() {
+//     if( $(this).width() > 600 ) {
+//         console.log('HI HI HI');
+//         // $('.flexslider').flexslider({
+//         //     controlNav: false,
+//         //     directionNav: true,
+//         //     touch: true,
+//         //     animation: "slide",
+//         //     controlsContainer: $(".custom-controls-container"),
+//         //     customDirectionNav: $(".custom-navigation a"),
+//         //     slideshow: false,
+//         //     });
+//     }
+// });
